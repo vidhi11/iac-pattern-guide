d480eda22bc7f7c04647c91556dd12f569febba9
Johandry Amador <Johandry.Amador@ibm.com>
2020-04-24 22:51:16 -0700

    01_Getting Started with Terraform

---
+++ 01-getting-started/output.tf
@@ -0,0 +1,3 @@
+output "ip_address" {
+  value = ibm_is_floating_ip.iac_test_floating_ip.address
+}
---
+++ 01-getting-started/variables.tf
@@ -0,0 +1,4 @@
+variable "public_key_file"  { default = "~/.ssh/id_rsa.pub" }
+locals {
+  public_key    = "${file(pathexpand(var.public_key_file))}"
+}
