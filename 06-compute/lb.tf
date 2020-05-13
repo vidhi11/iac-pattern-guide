04017411aef4cf64374e238cdd97b140bfeb17af
Johandry Amador <Johandry.Amador@ibm.com>
2020-05-12 20:22:21 -0700

    Fixing LB to the demo app

--- 06-compute/lb.tf
+++ 06-compute/lb.tf
@@ -7,24 +7,30 @@ resource "ibm_is_lb_listener" "iac_app_lb_listener" {
   lb       = ibm_is_lb.iac_app_lb.id
   port     = var.port
   protocol = "http"
+  default_pool = ibm_is_lb_pool.iac_app_lb_pool.id
 }
 
 resource "ibm_is_lb_pool" "iac_app_lb_pool" {
-  name           = "${var.project_name}-${var.environment}-lb-pool"
-  lb             = ibm_is_lb.iac_app_lb.id
-  algorithm      = "round_robin"
-  protocol       = "http"
-  health_delay   = 60
-  health_retries = 5
-  health_timeout = 30
-  health_type    = "http"
+  name                = "${var.project_name}-${var.environment}-lb-pool"
+  lb                  = ibm_is_lb.iac_app_lb.id
+  algorithm           = "round_robin"
+  protocol            = "http"
+  health_delay        = 5
+  health_retries      = 2
+  health_timeout      = 2
+  health_type         = "http"
+  health_monitor_url  = "/"
+  health_monitor_port = var.port
+  // depends_on         = [ ibm_is_lb_listener.iac_app_lb_listener ]
 }
 
 resource "ibm_is_lb_pool_member" "iac_app_lb_pool_mem" {
+  count          = var.max_size
   lb             = ibm_is_lb.iac_app_lb.id
   pool           = ibm_is_lb_pool.iac_app_lb_pool.id
   port           = var.port
   target_address = ibm_is_instance.iac_app_instance[count.index].primary_network_interface.0.primary_ipv4_address
-  weight         = 60
-  count          = var.max_size
+  weight         = (100 - count.index)
+  // pool           = element(split("/", ibm_is_lb_pool.iac_app_lb_pool.id), 1)
+  // depends_on     = [ ibm_is_lb_listener.iac_app_lb_listener ]
 }
