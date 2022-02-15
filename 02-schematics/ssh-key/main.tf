55ce8288a9d6f55b77e5f8e8d3a6b2b36170924c
Vidhi Shah <vidhi.se@gmail.com>
2022-02-15 13:54:56 -0600

    Update main.tf

--- 02-schematics/ssh-key/main.tf
+++ 02-schematics/ssh-key/main.tf
@@ -3,21 +3,15 @@ provider "ibm" {
   region             = "us-south"
 }
 
-variable "public_key_file"  { default = "~/.ssh/id_rsa.pub" }
-locals {
-  public_key    = file(pathexpand(var.public_key_file))
-}
-
 variable "ssh_key_name" {}
 
-resource "ibm_is_ssh_key" "iac_shared_ssh_key" {
+data "ibm_is_ssh_key" "iac_shared_ssh_key" {
   name       = var.ssh_key_name
-  public_key = local.public_key
 }
 
 output "id" {
-  value = ibm_is_ssh_key.iac_shared_ssh_key.id
+  value = data.ibm_is_ssh_key.iac_shared_ssh_key.id
 }
 output "ibm_cloud_url" {
-  value = ibm_is_ssh_key.iac_shared_ssh_key.resource_controller_url
+  value = data.ibm_is_ssh_key.iac_shared_ssh_key.resource_controller_url
 }
